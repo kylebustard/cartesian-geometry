470c5d22f2fd4e8277a071a4e35290bcf404e37b
Kyle Bustard <bustardkyle@gmail.com>
2019-09-28 21:39:18 -0500

    Validate line vs. point

--- cartesian-geometry.js
+++ cartesian-geometry.js
@@ -1,7 +1,17 @@
 function makeLine(input) {
     if (input.length === 2) {
-        return { type: 'LINE' };
+        if (input[0] == input[1]) {
+            return { type: 'ONE_POINT' };
+        } else {
+            return { type: 'LINE' };
+        }
     }
 }
 
-module.exports = { makeLine };
+function makeTriangle(input) {
+    if (input.length === 3) {
+        return { type: 'TRIANGLE' };
+    }
+}
+
+module.exports = { makeLine, makeTriangle };
--- cartesian-geometry.test.js
+++ cartesian-geometry.test.js
@@ -1,7 +1,25 @@
-const { makeLine } = require('./cartesian-geometry');
+const { makeLine, makeTriangle } = require('./cartesian-geometry');
 
 test('makes a line out of two points', () => {
-    const lineCoordinates = [[0, 0], [1, 1]];
+    const coordinates = [[0, 0], [1, 1]];
 
-    expect(makeLine(lineCoordinates)).toEqual({ type: 'LINE' });
-})
+    expect(makeLine(coordinates)).toEqual({ type: 'LINE' });
+});
+
+test('two ordered pairs that point to identical coordinates do not make a line', () => {
+    const coordinates = [[1, 1], [1, 1]];
+
+    expect(makeLine(coordinates)).not.toBe({ type: 'LINE' });
+});
+
+test('makes a triangle from three points', () => {
+    const coordinates = [[0, 0], [1, 1], [2, 2]];
+
+    expect(makeTriangle(coordinates)).toEqual({ type: 'TRIANGLE' });
+});
+
+xtest('a point is collinear if it lies on the same straight line', () => {
+    const coordinates = [[0, 0], [1, 1], [2, 2]];
+
+    expect(makeTriangle(coordinates)).toEqual({ type: 'LINE' });
+});
