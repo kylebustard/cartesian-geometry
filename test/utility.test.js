5c21b0d9df72fb3ee21e147062bd57b08bc7f602
Kyle Bustard <bustardkyle@gmail.com>
2019-10-03 18:46:28 -0500

    Reimplement `isPoint`

--- test/utility.test.js
+++
@@ -1,7 +0,0 @@
-const { wrapArgsInSingleArray } = require('../src/utility');
-
-xtest('wraps arguments in single array', () => {
-    const result = wrapArgsInSingleArray(1, 2);
-
-    expect(result.length).toBe(2);
-});
