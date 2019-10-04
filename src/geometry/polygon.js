1a822d069866f9b459d9be1b803cd9b25c946982
Kyle Bustard <bustardkyle@gmail.com>
2019-10-03 21:41:24 -0500

    make `isPoint` more robust with validating helper functions

--- src/geometry/polygon.js
+++ src/geometry/polygon.js
@@ -1,5 +1,39 @@
-function isPoint(orderedPair) {
-    return { type: 'POINT', coordinates: orderedPair };
+function isValidInput(input) {
+    if (!Array.isArray(input)) {
+        throw Error('input must be an Array');
+    } else if (arguments.length !== 1) {
+        throw Error('input must be a single Array');
+    } else if (input.length !== 2) {
+        throw Error('input must contain two values');
+    } else {
+        return input;
+    }
 }
 
-module.exports = { isPoint }
+function reduceIdenticalPairs(pairs) {
+    return { type: 'POINT', coordinates: [pairs[0]] };
+}
+
+function multiplePairs(pairs) {
+    for (let i = 0; i < pairs.length; i++) {
+        if (pairs[i][0] === pairs[i + 1][0] && pairs[i][1] === pairs[i + 1][1]) {
+            return reduceIdenticalPairs(pairs);
+        } else {
+            return { type: null, coordinates: [pairs[0], pairs[1]] }
+        }
+    }
+}
+
+function isPoint(orderedPair) {
+    if (!Array.isArray(orderedPair)) {
+        throw Error('input must be an Array');
+    }
+
+    if (orderedPair.length > 1) {
+        return multiplePairs(orderedPair);
+    }
+
+    return { type: 'POINT', coordinates: orderedPair }
+}
+
+module.exports = { isValidInput, isPoint }
--- test/geometryTest/polygon.test.js
+++ test/geometryTest/polygon.test.js
@@ -1,10 +1,52 @@
-const { isPoint } = require('../../src/geometry/polygon.js');
+const { isValidInput, isPoint } = require('../../src/geometry/polygon.js');
 
-describe('Point', () => {
-    it('returns an object containing `POINT` type and coordinates when given an ordered pair', () => {
-        const orderedPair = [[1, 2]];
-        const result = isPoint(orderedPair);
+describe('Input validation', () => {
+    describe('handling improper data', () => {
+        it('throws error when input is not an array', () => {
+            const result = () => isValidInput({ coordinates: [1, 2] });
 
-        expect(result).toEqual({ type: 'POINT', coordinates: orderedPair });
-    })
-})
+            expect(result).toThrowError(/input must be an Array/);
+        });
+
+        it('throws error when more than one input is received', () => {
+            const result = () => isValidInput([1, 2], [1, 2]);
+
+            expect(result).toThrowError(/input must be a single Array/);
+        })
+
+        it('throws error when input Array.length does not equal 2', () => {
+            const result = () => isValidInput([]);
+
+            expect(result).toThrowError(/input must contain two values/);
+        })
+    });
+
+    describe('ordered pair validation and preparation', () => {
+        it('throws error when input is not an array', () => {
+            const result = () => isPoint({ coordinates: [1, 2] });
+
+            expect(result).toThrowError(/input must be an Array/);
+        })
+
+        it('returns an object containing `POINT` type and coordinates when given a single ordered pair', () => {
+            const orderedPair = [[1, 2]];
+            const result = isPoint(orderedPair);
+
+            expect(result).toEqual({ type: 'POINT', coordinates: orderedPair });
+        });
+
+        describe('when given multiple ordered pairs', () => {
+            it('reduces identical ordered pairs to a single ordered pair', () => {
+                const result = isPoint([[1, 1], [1, 1]]);
+
+                expect(result.coordinates).toEqual([[1, 1]]);
+            });
+
+            it('assigns type `null` to a set of different ordered pairs', () => {
+                const result = isPoint([[1, 1], [1, 2]]);
+
+                expect(result).toEqual({ type: null, coordinates: [[1, 1], [1, 2]] });
+            });
+        });
+    });
+});
